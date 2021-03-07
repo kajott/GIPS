40c3017aa8d603e2e9a50337c01e398a1330a8ed
Martin Fiedler <keyj@emphy.de>
2021-03-07 23:24:01 +0100

    replaced sokol by SDL2
    sokol_gfx is basically a totally new graphics API
    to learn -- ain't got no time for that :) (And the
    main selling point of sokol_gfx, independence of the
    underlying API, is totally irrelevant here, as we're
    tied to GLSL anyway).
    
    So, let's bite the bullet and accept one DLL dependency
    (SDL2) for Win32 builds for now. At least, we can make
    the "out of box experience" somewhat nice by downloading
    a local copy of the SDL2 development package during
    the CMake run.

--- src/libs_cpp.cpp
+++
@@ -1,8 +0,0 @@
-#include "imgui.h"
-
-#include "sokol_app.h"
-#include "sokol_gfx.h"
-
-#define SOKOL_IMPL
-#define SOKOL_GLCORE33
-#include "sokol_imgui.h"
--- src/main.cpp
+++ src/main.cpp
@@ -1,52 +1,95 @@
+#include <cstdio>
+
+#include <SDL.h>
+#include <glad/glad.h>
+
 #include "imgui.h"
-#include "sokol_app.h"
-#include "sokol_gfx.h"
-#include "sokol_glue.h"
-#include "sokol_imgui.h"
+#include "imgui_impl_sdl.h"
+#include "imgui_impl_opengl3.h"
 
-sg_pass_action pass_action;
+int main(int argc, char *argv[]) {
+    (void) argc;
+    (void) argv;
 
-void OnInit() {
-    static sg_desc gfx_desc;
-    gfx_desc.context = sapp_sgcontext();
-    sg_setup(&gfx_desc);
+    if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER) < 0) {
+        fprintf(stderr, "SDL_Init failed: %s\n", SDL_GetError());
+        return 1;
+    }
 
-    pass_action.colors[0].action = SG_ACTION_CLEAR;
-    pass_action.colors[0].value = {1.0f, 0.0f, 0.0f, 1.0f};
+    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+    SDL_GL_SetAttribute(SDL_GL_RED_SIZE,     8);
+    SDL_GL_SetAttribute(SDL_GL_GREEN_SIZE,   8);
+    SDL_GL_SetAttribute(SDL_GL_BLUE_SIZE,    8);
+    SDL_GL_SetAttribute(SDL_GL_ALPHA_SIZE,   8);
+    SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE,   0);
+    SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 0);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
 
-    static simgui_desc_t imgui_desc;
-    simgui_setup(&imgui_desc);
-}
+    SDL_Window *window = SDL_CreateWindow("GIPS",
+        SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+        800, 600,
+        SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE | SDL_WINDOW_ALLOW_HIGHDPI);
+    if (window == nullptr) {
+        fprintf(stderr, "SDL_CreateWindow failed: %s\n", SDL_GetError());
+        return 1;
+    }
 
-void OnFrame() {
-    float g = pass_action.colors[0].value.g + 0.01f;
-    pass_action.colors[0].value.g = (g > 1.0f) ? 0.0f : g;
-    sg_begin_default_pass(&pass_action, sapp_width(), sapp_height());
-    simgui_new_frame(sapp_width(), sapp_height(), 1.0/60);
-    ImGui::Text("Hello World!");
-    simgui_render();
-    sg_end_pass();
-    sg_commit();
-}
+    SDL_GLContext context = SDL_GL_CreateContext(window);
+    if (context == nullptr) {
+        fprintf(stderr, "SDL_GL_CreateContext failed: %s\n", SDL_GetError());
+        return 1;
+    }
 
-void OnEvent(const sapp_event* ev) {
-    simgui_handle_event(ev);
-}
+    SDL_GL_MakeCurrent(window, context);
+    SDL_GL_SetSwapInterval(1);
 
-void OnQuit() {
-    simgui_shutdown();
-    sg_shutdown();
-}
+    if (!gladLoadGLLoader((GLADloadproc)SDL_GL_GetProcAddress)) {
+        fprintf(stderr, "failed to load OpenGL 3.3 functions\n");
+        return 1;
+    }
 
-extern "C"
-sapp_desc sokol_main(int argc, char* argv[]) {
-    static sapp_desc app_desc;
-    app_desc.init_cb = OnInit;
-    app_desc.frame_cb = OnFrame;
-    app_desc.event_cb = OnEvent;
-    app_desc.cleanup_cb = OnQuit;
-    app_desc.width = 800;
-    app_desc.height = 600;
-    app_desc.window_title = "GIPS";
-    return app_desc;
+    glClearColor(0.1f, 0.2f, 0.3f, 1.0f);
+
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO();
+    ImGui_ImplSDL2_InitForOpenGL(window, context);
+    ImGui_ImplOpenGL3_Init(NULL);
+
+    for (bool active = true;  active;) {
+        SDL_Event ev;
+        while (SDL_PollEvent(&ev)) {
+            ImGui_ImplSDL2_ProcessEvent(&ev);
+            switch (ev.type) {
+                case SDL_QUIT:
+                    active = false;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplSDL2_NewFrame(window);
+        ImGui::NewFrame();
+
+        ImGui::Text("Hi from ImGui!"); 
+
+        ImGui::Render();
+
+        glViewport(0, 0, int(io.DisplaySize.x), int(io.DisplaySize.y));
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+        SDL_GL_SwapWindow(window);
+    }
+
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplSDL2_Shutdown();
+    ImGui::DestroyContext();
+    SDL_GL_DeleteContext(context);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+    return 0;
 }
