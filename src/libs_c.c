fed7ba82d31afcf1914039e31913f9e77c218b59
Martin Fiedler <keyj@emphy.de>
2021-03-20 23:45:36 +0100

    using a sharper downsampling filter

--- src/libs_c.c
+++ src/libs_c.c
@@ -5,4 +5,5 @@
 #include "stb_image_write.h"
 
 #define STB_IMAGE_RESIZE_IMPLEMENTATION
+#define STBIR_DEFAULT_FILTER_DOWNSAMPLE STBIR_FILTER_BOX  // for sharper images
 #include "stb_image_resize.h"
