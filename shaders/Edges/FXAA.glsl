dfd196fb03804ebb92f5e872becb173a6a99cd99
Martin Fiedler <keyj@emphy.de>
2021-03-29 23:06:57 +0200

    refactored FXAA shader
    The original code was clearly written with very dumb,
    non-optimizing compilers in mind: It's almost SSA form
    (declaring temporary variables for almost all
    intermediate computations), doesn't use vector operations
    where appropriate, and the elephant in the room
    is the 12 levels deeply nested if{} chain.
    
    The if chain has been replaced by a simple loop,
    and lots of intermediate computations have been
    moved into the places where the results
    are actually needed.
    
    The end result is almost 70% shorter than
    the previous version (which was already
    a few thousands of lines shorter than
    the original version).

--- shaders/Edges/FXAA.glsl
+++ shaders/Edges/FXAA.glsl
@@ -25,234 +25,58 @@ vec4 run_pass2(vec2 posM) {
     float lumaE = textureLodOffset(gips_tex, posM, 0.0, ivec2( 1, 0)).w;
     float lumaN = textureLodOffset(gips_tex, posM, 0.0, ivec2( 0,-1)).w;
     float lumaW = textureLodOffset(gips_tex, posM, 0.0, ivec2(-1, 0)).w;
-    float maxSM = max(lumaS, rgbyM.w);
-    float minSM = min(lumaS, rgbyM.w);
-    float maxESM = max(lumaE, maxSM);
-    float minESM = min(lumaE, minSM);
-    float maxWN = max(lumaN, lumaW);
-    float minWN = min(lumaN, lumaW);
-    float rangeMax = max(maxWN, maxESM);
-    float rangeMin = min(minWN, minESM);
-    float rangeMaxScaled = rangeMax * pEdgeThresh;
+    float rangeMax = max(max(lumaN, lumaW), max(lumaE, max(lumaS, rgbyM.w)));
+    float rangeMin = min(min(lumaN, lumaW), min(lumaE, min(lumaS, rgbyM.w)));
     float range = rangeMax - rangeMin;
-    float rangeMaxClamped = max(pEdgeMin, rangeMaxScaled);
-    bool earlyExit = range < rangeMaxClamped;
-    if(earlyExit) { return vec4(rgbyM.rgb, 1.0); }
+    if (range < max(pEdgeMin, rangeMax * pEdgeThresh)) { return vec4(rgbyM.rgb, 1.0); }
     float lumaNW = textureLodOffset(gips_tex, posM, 0.0, ivec2(-1,-1)).w;
     float lumaSE = textureLodOffset(gips_tex, posM, 0.0, ivec2( 1, 1)).w;
     float lumaNE = textureLodOffset(gips_tex, posM, 0.0, ivec2( 1,-1)).w;
     float lumaSW = textureLodOffset(gips_tex, posM, 0.0, ivec2(-1, 1)).w;
     float lumaNS = lumaN + lumaS;
     float lumaWE = lumaW + lumaE;
-    float subpixRcpRange = 1.0/range;
-    float subpixNSWE = lumaNS + lumaWE;
-    float edgeHorz1 = (-2.0 * rgbyM.w) + lumaNS;
-    float edgeVert1 = (-2.0 * rgbyM.w) + lumaWE;
     float lumaNESE = lumaNE + lumaSE;
     float lumaNWNE = lumaNW + lumaNE;
-    float edgeHorz2 = (-2.0 * lumaE) + lumaNESE;
-    float edgeVert2 = (-2.0 * lumaN) + lumaNWNE;
     float lumaNWSW = lumaNW + lumaSW;
     float lumaSWSE = lumaSW + lumaSE;
-    float edgeHorz4 = (abs(edgeHorz1) * 2.0) + abs(edgeHorz2);
-    float edgeVert4 = (abs(edgeVert1) * 2.0) + abs(edgeVert2);
-    float edgeHorz3 = (-2.0 * lumaW) + lumaNWSW;
-    float edgeVert3 = (-2.0 * lumaS) + lumaSWSE;
-    float edgeHorz = abs(edgeHorz3) + edgeHorz4;
-    float edgeVert = abs(edgeVert3) + edgeVert4;
-    float subpixNWSWNESE = lumaNWSW + lumaNESE;
-    float lengthSign = rcpFrame.x;
+    float subpixA = (lumaNS + lumaWE) * 2.0 + (lumaNWSW + lumaNESE);
+    subpixA = clamp(abs((subpixA * (1.0/12.0)) - rgbyM.w) / range, 0.0, 1.0);
+    subpixA = (3.0 - 2.0 * subpixA) * (subpixA * subpixA);
+    float edgeHorz = abs(lumaNWSW - 2.0 * lumaW) + 2.0 * abs(lumaNS - 2.0 * rgbyM.w) + abs(lumaNESE - 2.0 * lumaE);
+    float edgeVert = abs(lumaSWSE - 2.0 * lumaS) + 2.0 * abs(lumaWE - 2.0 * rgbyM.w) + abs(lumaNWNE - 2.0 * lumaN);
     bool horzSpan = edgeHorz >= edgeVert;
-    float subpixA = subpixNSWE * 2.0 + subpixNWSWNESE;
-    if(!horzSpan) lumaN = lumaW;
-    if(!horzSpan) lumaS = lumaE;
-    if(horzSpan) lengthSign = rcpFrame.y;
-    float subpixB = (subpixA * (1.0/12.0)) - rgbyM.w;
-    float gradientN = lumaN - rgbyM.w;
-    float gradientS = lumaS - rgbyM.w;
-    float lumaNN = lumaN + rgbyM.w;
-    float lumaSS = lumaS + rgbyM.w;
-    bool pairN = abs(gradientN) >= abs(gradientS);
-    float gradient = max(abs(gradientN), abs(gradientS));
-    if(pairN) lengthSign = -lengthSign;
-    float subpixC = clamp(abs(subpixB) * subpixRcpRange, 0.0, 1.0);
+    float lengthSign = horzSpan ? rcpFrame.y : rcpFrame.x;
+    if (!horzSpan) { lumaN = lumaW; lumaS = lumaE; }
+    float gradientN = abs(lumaN - rgbyM.w);
+    float gradientS = abs(lumaS - rgbyM.w);
+    float gradient = max(gradientN, gradientS) * 0.25;
+    bool pairN = (gradientN >= gradientS);
+    if (pairN) { lengthSign = -lengthSign; }
+    float lumaNN = 0.5 * ((pairN ? lumaN : lumaS) + rgbyM.w);
+    vec2 offNP = horzSpan ? vec2(rcpFrame.x, 0.0) : vec2(0.0, rcpFrame.y);
     vec2 posB = posM;
-    vec2 offNP;
-    offNP.x = (!horzSpan) ? 0.0 : rcpFrame.x;
-    offNP.y = ( horzSpan) ? 0.0 : rcpFrame.y;
-    if(!horzSpan) posB.x += lengthSign * 0.5;
-    if( horzSpan) posB.y += lengthSign * 0.5;
-    vec2 posN;
-    posN.x = posB.x - offNP.x * 1.0;
-    posN.y = posB.y - offNP.y * 1.0;
-    vec2 posP;
-    posP.x = posB.x + offNP.x * 1.0;
-    posP.y = posB.y + offNP.y * 1.0;
-    float subpixD = ((-2.0)*subpixC) + 3.0;
-    float lumaEndN = textureLod(gips_tex, posN, 0.0).w;
-    float subpixE = subpixC * subpixC;
-    float lumaEndP = textureLod(gips_tex, posP, 0.0).w;
-    if(!pairN) lumaNN = lumaSS;
-    float gradientScaled = gradient * 1.0/4.0;
-    float lumaMM = rgbyM.w - lumaNN * 0.5;
-    float subpixF = subpixD * subpixE;
-    bool lumaMLTZero = lumaMM < 0.0;
-    lumaEndN -= lumaNN * 0.5;
-    lumaEndP -= lumaNN * 0.5;
-    bool doneN = abs(lumaEndN) >= gradientScaled;
-    bool doneP = abs(lumaEndP) >= gradientScaled;
-    if(!doneN) posN.x -= offNP.x * 1.0;
-    if(!doneN) posN.y -= offNP.y * 1.0;
-    bool doneNP = (!doneN) || (!doneP);
-    if(!doneP) posP.x += offNP.x * 1.0;
-    if(!doneP) posP.y += offNP.y * 1.0;
-    if(doneNP) {
-        if(!doneN) lumaEndN = textureLod(gips_tex, posN.xy, 0.0).w;
-        if(!doneP) lumaEndP = textureLod(gips_tex, posP.xy, 0.0).w;
-        if(!doneN) lumaEndN = lumaEndN - lumaNN * 0.5;
-        if(!doneP) lumaEndP = lumaEndP - lumaNN * 0.5;
-        doneN = abs(lumaEndN) >= gradientScaled;
-        doneP = abs(lumaEndP) >= gradientScaled;
-        if(!doneN) posN.x -= offNP.x * 1.0;
-        if(!doneN) posN.y -= offNP.y * 1.0;
-        doneNP = (!doneN) || (!doneP);
-        if(!doneP) posP.x += offNP.x * 1.0;
-        if(!doneP) posP.y += offNP.y * 1.0;
-        if(doneNP) {
-            if(!doneN) lumaEndN = textureLod(gips_tex, posN.xy, 0.0).w;
-            if(!doneP) lumaEndP = textureLod(gips_tex, posP.xy, 0.0).w;
-            if(!doneN) lumaEndN = lumaEndN - lumaNN * 0.5;
-            if(!doneP) lumaEndP = lumaEndP - lumaNN * 0.5;
-            doneN = abs(lumaEndN) >= gradientScaled;
-            doneP = abs(lumaEndP) >= gradientScaled;
-            if(!doneN) posN.x -= offNP.x * 1.0;
-            if(!doneN) posN.y -= offNP.y * 1.0;
-            doneNP = (!doneN) || (!doneP);
-            if(!doneP) posP.x += offNP.x * 1.0;
-            if(!doneP) posP.y += offNP.y * 1.0;
-            if(doneNP) {
-                if(!doneN) lumaEndN = textureLod(gips_tex, posN.xy, 0.0).w;
-                if(!doneP) lumaEndP = textureLod(gips_tex, posP.xy, 0.0).w;
-                if(!doneN) lumaEndN = lumaEndN - lumaNN * 0.5;
-                if(!doneP) lumaEndP = lumaEndP - lumaNN * 0.5;
-                doneN = abs(lumaEndN) >= gradientScaled;
-                doneP = abs(lumaEndP) >= gradientScaled;
-                if(!doneN) posN.x -= offNP.x * 1.0;
-                if(!doneN) posN.y -= offNP.y * 1.0;
-                doneNP = (!doneN) || (!doneP);
-                if(!doneP) posP.x += offNP.x * 1.0;
-                if(!doneP) posP.y += offNP.y * 1.0;
-                if(doneNP) {
-                    if(!doneN) lumaEndN = textureLod(gips_tex, posN.xy, 0.0).w;
-                    if(!doneP) lumaEndP = textureLod(gips_tex, posP.xy, 0.0).w;
-                    if(!doneN) lumaEndN = lumaEndN - lumaNN * 0.5;
-                    if(!doneP) lumaEndP = lumaEndP - lumaNN * 0.5;
-                    doneN = abs(lumaEndN) >= gradientScaled;
-                    doneP = abs(lumaEndP) >= gradientScaled;
-                    if(!doneN) posN.x -= offNP.x * 1.5;
-                    if(!doneN) posN.y -= offNP.y * 1.5;
-                    doneNP = (!doneN) || (!doneP);
-                    if(!doneP) posP.x += offNP.x * 1.5;
-                    if(!doneP) posP.y += offNP.y * 1.5;
-                    if(doneNP) {
-                        if(!doneN) lumaEndN = textureLod(gips_tex, posN.xy, 0.0).w;
-                        if(!doneP) lumaEndP = textureLod(gips_tex, posP.xy, 0.0).w;
-                        if(!doneN) lumaEndN = lumaEndN - lumaNN * 0.5;
-                        if(!doneP) lumaEndP = lumaEndP - lumaNN * 0.5;
-                        doneN = abs(lumaEndN) >= gradientScaled;
-                        doneP = abs(lumaEndP) >= gradientScaled;
-                        if(!doneN) posN.x -= offNP.x * 2.0;
-                        if(!doneN) posN.y -= offNP.y * 2.0;
-                        doneNP = (!doneN) || (!doneP);
-                        if(!doneP) posP.x += offNP.x * 2.0;
-                        if(!doneP) posP.y += offNP.y * 2.0;
-                        if(doneNP) {
-                            if(!doneN) lumaEndN = textureLod(gips_tex, posN.xy, 0.0).w;
-                            if(!doneP) lumaEndP = textureLod(gips_tex, posP.xy, 0.0).w;
-                            if(!doneN) lumaEndN = lumaEndN - lumaNN * 0.5;
-                            if(!doneP) lumaEndP = lumaEndP - lumaNN * 0.5;
-                            doneN = abs(lumaEndN) >= gradientScaled;
-                            doneP = abs(lumaEndP) >= gradientScaled;
-                            if(!doneN) posN.x -= offNP.x * 2.0;
-                            if(!doneN) posN.y -= offNP.y * 2.0;
-                            doneNP = (!doneN) || (!doneP);
-                            if(!doneP) posP.x += offNP.x * 2.0;
-                            if(!doneP) posP.y += offNP.y * 2.0;
-                            if(doneNP) {
-                                if(!doneN) lumaEndN = textureLod(gips_tex, posN.xy, 0.0).w;
-                                if(!doneP) lumaEndP = textureLod(gips_tex, posP.xy, 0.0).w;
-                                if(!doneN) lumaEndN = lumaEndN - lumaNN * 0.5;
-                                if(!doneP) lumaEndP = lumaEndP - lumaNN * 0.5;
-                                doneN = abs(lumaEndN) >= gradientScaled;
-                                doneP = abs(lumaEndP) >= gradientScaled;
-                                if(!doneN) posN.x -= offNP.x * 2.0;
-                                if(!doneN) posN.y -= offNP.y * 2.0;
-                                doneNP = (!doneN) || (!doneP);
-                                if(!doneP) posP.x += offNP.x * 2.0;
-                                if(!doneP) posP.y += offNP.y * 2.0;
-                                if(doneNP) {
-                                    if(!doneN) lumaEndN = textureLod(gips_tex, posN.xy, 0.0).w;
-                                    if(!doneP) lumaEndP = textureLod(gips_tex, posP.xy, 0.0).w;
-                                    if(!doneN) lumaEndN = lumaEndN - lumaNN * 0.5;
-                                    if(!doneP) lumaEndP = lumaEndP - lumaNN * 0.5;
-                                    doneN = abs(lumaEndN) >= gradientScaled;
-                                    doneP = abs(lumaEndP) >= gradientScaled;
-                                    if(!doneN) posN.x -= offNP.x * 2.0;
-                                    if(!doneN) posN.y -= offNP.y * 2.0;
-                                    doneNP = (!doneN) || (!doneP);
-                                    if(!doneP) posP.x += offNP.x * 2.0;
-                                    if(!doneP) posP.y += offNP.y * 2.0;
-                                    if(doneNP) {
-                                        if(!doneN) lumaEndN = textureLod(gips_tex, posN.xy, 0.0).w;
-                                        if(!doneP) lumaEndP = textureLod(gips_tex, posP.xy, 0.0).w;
-                                        if(!doneN) lumaEndN = lumaEndN - lumaNN * 0.5;
-                                        if(!doneP) lumaEndP = lumaEndP - lumaNN * 0.5;
-                                        doneN = abs(lumaEndN) >= gradientScaled;
-                                        doneP = abs(lumaEndP) >= gradientScaled;
-                                        if(!doneN) posN.x -= offNP.x * 4.0;
-                                        if(!doneN) posN.y -= offNP.y * 4.0;
-                                        doneNP = (!doneN) || (!doneP);
-                                        if(!doneP) posP.x += offNP.x * 4.0;
-                                        if(!doneP) posP.y += offNP.y * 4.0;
-                                        if(doneNP) {
-                                            if(!doneN) lumaEndN = textureLod(gips_tex, posN.xy, 0.0).w;
-                                            if(!doneP) lumaEndP = textureLod(gips_tex, posP.xy, 0.0).w;
-                                            if(!doneN) lumaEndN = lumaEndN - lumaNN * 0.5;
-                                            if(!doneP) lumaEndP = lumaEndP - lumaNN * 0.5;
-                                            doneN = abs(lumaEndN) >= gradientScaled;
-                                            doneP = abs(lumaEndP) >= gradientScaled;
-                                            if(!doneN) posN.x -= offNP.x * 8.0;
-                                            if(!doneN) posN.y -= offNP.y * 8.0;
-                                            doneNP = (!doneN) || (!doneP);
-                                            if(!doneP) posP.x += offNP.x * 8.0;
-                                            if(!doneP) posP.y += offNP.y * 8.0;
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }
+    if (horzSpan) { posB.y += lengthSign * 0.5; }
+             else { posB.x += lengthSign * 0.5; }
+    vec2 posN = posB - offNP;
+    vec2 posP = posB + offNP;
+    bool doneN = false, doneP = false;
+    float lumaEndN, lumaEndP;
+    const float stepSizes[12] = float[](1.0, 1.0, 1.0, 1.0, 1.0, 1.5, 2.0, 2.0, 2.0, 2.0, 4.0, 8.0);
+    for (int step = 0;  step < 12;  ++step) {
+        if (!doneN) { lumaEndN = textureLod(gips_tex, posN, 0.0).w - lumaNN; }
+        if (!doneP) { lumaEndP = textureLod(gips_tex, posP, 0.0).w - lumaNN; }
+        doneN = abs(lumaEndN) >= gradient;
+        doneP = abs(lumaEndP) >= gradient;
+        if (!doneN) { posN -= offNP * stepSizes[step]; }
+        if (!doneP) { posP += offNP * stepSizes[step]; }
+        if (doneN && doneP) { break; }
     }
-    float dstN = posM.x - posN.x;
-    float dstP = posP.x - posM.x;
-    if(!horzSpan) dstN = posM.y - posN.y;
-    if(!horzSpan) dstP = posP.y - posM.y;
-    bool goodSpanN = (lumaEndN < 0.0) != lumaMLTZero;
-    float spanLength = (dstP + dstN);
-    bool goodSpanP = (lumaEndP < 0.0) != lumaMLTZero;
-    float spanLengthRcp = 1.0/spanLength;
-    bool directionN = dstN < dstP;
-    float dst = min(dstN, dstP);
-    bool goodSpan = directionN ? goodSpanN : goodSpanP;
-    float subpixG = subpixF * subpixF;
-    float pixelOffset = (dst * (-spanLengthRcp)) + 0.5;
-    float subpixH = subpixG * pSubpix;
-    float pixelOffsetGood = goodSpan ? pixelOffset : 0.0;
-    float pixelOffsetSubpix = max(pixelOffsetGood, subpixH);
-    if(!horzSpan) posM.x += pixelOffsetSubpix * lengthSign;
-    if( horzSpan) posM.y += pixelOffsetSubpix * lengthSign;
+    float dstN = horzSpan ? (posM.x - posN.x) : (posM.y - posN.y);
+    float dstP = horzSpan ? (posP.x - posM.x) : (posP.y - posN.y);
+    bool goodSpan = (((dstN < dstP) ? lumaEndN : lumaEndP) < 0.0) != ((rgbyM.w - lumaNN) < 0.0);
+    float pixelOffset = 0.5 - min(dstN, dstP) / (dstN + dstP);
+    float pixelOffsetSubpix = max(goodSpan ? pixelOffset : 0.0, (subpixA * subpixA) * pSubpix);
+    if (horzSpan) { posM.y += pixelOffsetSubpix * lengthSign; }
+             else { posM.x += pixelOffsetSubpix * lengthSign; }
     return vec4(textureLod(gips_tex, posM, 0.0).xyz, 1.0);
 }
