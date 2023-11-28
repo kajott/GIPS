4b78796beac8ae9f8a07848dadeecb6e03b72146
Martin Fiedler <keyj@emphy.de>
2023-11-28 17:45:37 +0100

    Blur+Sharpen shader with adjustable amount of clipping/ringing
    Sometimes, allowing a bit of ringing
    is a good compromise.

--- shaders/Blur+Sharpen/Simple Blur+Sharpen.glsl
+++ shaders/Blur+Sharpen/Simple Blur+Sharpen.glsl
@@ -1,11 +1,11 @@
-// SPDX-FileCopyrightText: 2021 Martin J. Fiedler <keyj@emphy.de>
+// SPDX-FileCopyrightText: 2021-2023 Martin J. Fiedler <keyj@emphy.de>
 // SPDX-License-Identifier: MIT
 
 // @gips_version=1 @coord=none @filter=off
 
 uniform float blurriness;       // @min=-5 @max=1
 uniform float threshold = 1.0;  // @digits=3
-uniform float clip;             // @switch avoid ringing when sharpening
+uniform float ringing   = 1.0;  // ringing allowed
 
 vec4 run(vec2 pos) {
     vec3 p00 = textureLodOffset(gips_tex, pos, 0.0, ivec2(-1,-1)).rgb;
@@ -20,10 +20,10 @@ vec4 run(vec2 pos) {
     vec3 res = 0.25 * p11.rgb + 0.125 * (p01 + p10 + p12 + p21) + 0.0625 * (p00 + p02 + p20 + p22);
     res = mix(p11.rgb, res, blurriness);
     res = mix(p11.rgb, res, lessThan(abs(res - p11.rgb), vec3(threshold)));
-    if ((clip > 0.5) && (blurriness < 0.0)) {
+    if ((ringing < 1.0) && (blurriness < 0.0)) {
         vec3 vmin = min(min(min(min(p00, p22), min(p20, p02)), min(min(p01, p10), min(p21, p12))), p11.rgb);
         vec3 vmax = max(max(max(max(p00, p22), max(p20, p02)), max(max(p01, p10), max(p21, p12))), p11.rgb);
-        res = min(max(res, vmin), vmax);
+        res = mix(min(max(res, vmin), vmax), res, ringing);
     }
     return vec4(res, p11.a);
 }
